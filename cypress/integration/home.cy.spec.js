bfcc9081645fcdfc44da27927b9cb3db132689b6
Marinus Eigenraam <marinus.eigenraam@finalist.nl>
2022-11-04 10:32:35 +0100

    start of tutorial

---
+++ cypress/integration/home.cy.spec.js
@@ -0,0 +1,11 @@
+describe("home page", () => {
+  it( "the h1 contains the correct text", () => {
+    cy.visit( "http://localhost:3000" )
+    cy.get( "[data-test='hero-heading']" ).contains(
+        "Testing Next.js Applications with Cypress"
+    )
+  } );
+  it("the features on the homepage are correct", () => {
+    cy.visit("http://localhost:3000")
+  })
+})
---
+++ cypress/integration/home.own.spec.ts
@@ -0,0 +1,14 @@
+describe('empty spec', () => {
+  it('passes', () => {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
+
+    cy.get('.action-email')
+        .type('fake@email.com')
+        .should('have.value','fake@email.com')
+
+  })
+})
